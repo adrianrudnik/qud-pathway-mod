9984d170551291ee6d60611a35141b77c3466fb8
Adrian Rudnik <adrian@klonmaschine.de>
2024-11-02 00:26:57 +0100

    first learning draft

---
+++ Pathway_Init.cs
@@ -0,0 +1,15 @@
+﻿
+using XRL;
+
+namespace Pathway.Handlers
+{
+    [HasCallAfterGameLoaded]
+    public class LoadGameHandler
+    {
+        [CallAfterGameLoaded]
+        public static void AfterLoaded()
+        {
+            The.Player.RequirePart<Pathway_Radio>();
+        }      
+    }
+}
